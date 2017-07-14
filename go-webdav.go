b74b53175ebc4f998ee8d223ad8e79cdcf29f6c0
Patrick <devnull@mailinator.com>
2017-07-14 13:57:39 +0800

    add basic auth

--- go-webdav.go
+++ go-webdav.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
 	"golang.org/x/net/webdav"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const (
@@ -38,12 +40,43 @@ func filesystem() webdav.FileSystem {
 	if err := os.Mkdir(pathRoot, os.ModePerm); !os.IsExist(err) {
 		log.Fatalf("FATAL %v", err)
 	}
-	log.Printf("INFO using local filesystem at %s", pathRoot)
+	log.Printf("INFO using local filesystem at %s\n", pathRoot)
 	return webdav.Dir(pathRoot)
 }
 
-func router(h *Handler) func(w http.ResponseWriter, r *http.Request) {
-	h.ServeHTTP(w, r)
+func isAuth(w http.ResponseWriter, r *http.Request) bool {
+	w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+
+	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(s) != 2 {
+		return false
+	}
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		return false
+	}
+
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 {
+		return false
+	}
+
+	if pair[0] != "username" && pair[1] != "password" {
+		return false
+	}
+
+	return true
+}
+
+func router(h *webdav.Handler) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if isAuth(w, r) == true {
+			h.ServeHTTP(w, r)
+		} else {
+			http.Error(w, "Not authorized", 401)
+		}
+	}
 }
 
 func main() {
@@ -63,5 +96,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", router(h))
-	http.ListenAndServe("127.0.0.1:8080", h)
+	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
+		log.Fatalf("Cannot bind: %v", err)
+	}
 }
